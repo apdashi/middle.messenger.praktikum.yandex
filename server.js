27ca1176bcd4e86b06aeaa6818f474e181452aa4
apdashi <apdashi@ya.ru>
2023-06-20 09:41:36 +0300

    Спринт 3

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static('./dist/'))
-app.use('*', (req, res) => {
-    res.redirect('/')
-})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + './dist/index.html')
-})
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`)
 })
